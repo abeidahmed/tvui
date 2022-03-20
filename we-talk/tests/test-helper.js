5a7502aba5ec713a8afcec189a6c6703b5169ae4
abeidahmed <abeidahmed92@gmail.com>
2022-03-20 18:25:26 +0530

    Initial commit

---
+++ we-talk/tests/test-helper.js
@@ -0,0 +1,12 @@
+import Application from 'we-talk/app';
+import config from 'we-talk/config/environment';
+import * as QUnit from 'qunit';
+import { setApplication } from '@ember/test-helpers';
+import { setup } from 'qunit-dom';
+import { start } from 'ember-qunit';
+
+setApplication(Application.create(config.APP));
+
+setup(QUnit.assert);
+
+start();
