b9700b9bc8c984d9a7d53a8448636029e78c1023
abeidahmed <abeidahmed92@gmail.com>
2022-03-26 15:24:32 +0530

    feat: install tailwind

--- we-talk/ember-cli-build.js
+++ we-talk/ember-cli-build.js
@@ -4,7 +4,14 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function (defaults) {
   let app = new EmberApp(defaults, {
-    // Add options here
+    postcssOptions: {
+      compile: {
+        plugins: [
+          { module: require('postcss-import') },
+          require('tailwindcss')('./tailwind.config.js'),
+        ],
+      },
+    },
   });
 
   // Use `app.import` to add additional libraries to the generated
